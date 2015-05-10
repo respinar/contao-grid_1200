f1ea86aaf91eb709e58f5db09b69991ba765d5d3
hamidabbaszadeh <abbaszadeh.h@gmail.com>
2015-05-10 22:27:54 +0430

    fix

--- system/modules/grid_1200/languages/en/tl_layout.php
+++ system/modules/grid_1200/languages/en/tl_layout.php
@@ -11,11 +11,11 @@
  * last-updated: 2014-06-08T22:51:20+02:00
  */
 
-$GLOBALS['TL_LANG']['tl_layout']['grid-1200-12-ltr']['css']['0']         = '12-column  grid, 1200px, ltr';
-$GLOBALS['TL_LANG']['tl_layout']['grid-1200-12-ltr']['css']['1']         = 'Adds a ltr 12-column grid with 1200px, that is triggered by the CSS classes "grid1" to "grid12" and "offset1" to "offset11".';
-$GLOBALS['TL_LANG']['tl_layout']['grid-1200-12-ltr-wrapper']['css']['0'] = '12-column  grid, 1200px, ltr, wrapper';
-$GLOBALS['TL_LANG']['tl_layout']['grid-1200-12-ltr-wrapper']['css']['1'] = 'Adds a ltr 12-column grid with 1200px wrapper, that is triggered by the CSS classes "grid1" to "grid12" and "offset1" to "offset11".';
-$GLOBALS['TL_LANG']['tl_layout']['grid-1200-12-rtl']['css']['0']         = '12-column grid, 1200px, rtl';
-$GLOBALS['TL_LANG']['tl_layout']['grid-1200-12-rtl']['css']['1']         = 'Adds a rtl 12-column grid with 1200px, that is triggered by the CSS classes "grid1" to "grid12" and "offset1" to "offset11".';
-$GLOBALS['TL_LANG']['tl_layout']['grid-1200-12-rtl-wrapper']['css']['0'] = '12-column grid, percent, 1200px, rtl, wrapper';
-$GLOBALS['TL_LANG']['tl_layout']['grid-1200-12-rtl-wrapper']['css']['1'] = 'Adds a rtl 12-column grid with 1200px wrapper, that is triggered by the CSS classes "grid1" to "grid12" and "offset1" to "offset11".';
+$GLOBALS['TL_LANG']['tl_layout']['grid-1200-12-ltr']['css'][0]         = '12-column  grid, 1200px, ltr';
+$GLOBALS['TL_LANG']['tl_layout']['grid-1200-12-ltr']['css'][1]         = 'Adds a ltr 12-column grid with 1200px, that is triggered by the CSS classes "grid1" to "grid12" and "offset1" to "offset11".';
+$GLOBALS['TL_LANG']['tl_layout']['grid-1200-12-ltr-wrapper']['css'][0] = '12-column  grid, 1200px, ltr, wrapper';
+$GLOBALS['TL_LANG']['tl_layout']['grid-1200-12-ltr-wrapper']['css'][1] = 'Adds a ltr 12-column grid with 1200px wrapper, that is triggered by the CSS classes "grid1" to "grid12" and "offset1" to "offset11".';
+$GLOBALS['TL_LANG']['tl_layout']['grid-1200-12-rtl']['css'][0]         = '12-column grid, 1200px, rtl';
+$GLOBALS['TL_LANG']['tl_layout']['grid-1200-12-rtl']['css'][1]         = 'Adds a rtl 12-column grid with 1200px, that is triggered by the CSS classes "grid1" to "grid12" and "offset1" to "offset11".';
+$GLOBALS['TL_LANG']['tl_layout']['grid-1200-12-rtl-wrapper']['css'][0] = '12-column grid, percent, 1200px, rtl, wrapper';
+$GLOBALS['TL_LANG']['tl_layout']['grid-1200-12-rtl-wrapper']['css'][1] = 'Adds a rtl 12-column grid with 1200px wrapper, that is triggered by the CSS classes "grid1" to "grid12" and "offset1" to "offset11".';
